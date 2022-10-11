c1187765935c7f477720b50b21db29bb3f4b535e
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-10-11 16:51:59 -0300

    templates/estilo/

---
+++ data/posts.php
@@ -0,0 +1,34 @@
+<?php
+
+  $posts = [
+    [
+      'id' => 1,
+      'title' => 'Por que levar o seu pet no Veterinário regularmente?',
+      'description' => 'Você sabe a importância de levar o seu pet ao veterinário regularmente?',
+      'tags' => ['veterinário', 'Cuidados', 'Prevenção'],
+      'img' => 'vet.jpg'
+    ],
+    [
+      'id' => 2,
+      'title' => 'Como cuidar de filhotes recém nascidos',
+      'description' => 'Encontrou um filhote recém nascido? seu animal de estimação deu a luz a filhotes? Saiba como proceder',
+      'tags' => ['Filhotes', 'Cuidados', 'Recém nascidos'],
+      'img' => 'nascidos.jpg'
+    ],
+    [
+      'id' => 3,
+      'title' => 'A importância da Vacinação em cães e gatos',
+      'description' => 'Descubra o porque manter as vacinas do seu pet em dia é super importante',
+      'tags' => ['Vacina', 'Prevenção', 'Doenças'],
+      'img' => 'vacina.jpg',
+    ],
+    [
+      'id' => 4,
+      'title' => 'Cuidados com cães idosos',
+      'description' => 'Descubra como cuidar do seu cão idoso',
+      'tags' => ['Idosos', 'Cuidados', 'Exames'],
+      'img' => 'idosos.jpg'
+    ],
+  ];
+
+?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Funny Pet &copy; 2022</p>
+</footer>
+
+</body>
+</html>
